c4c43bbacff335222cd24a8e9c45ad140cd47d20
Joshua Wise <joshua@joshuawise.com>
2020-03-15 03:16:03 -0400

    Convert to setuptools.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rws-common',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'honeycomb-beeline==2.11.4'
+    ]
+)
