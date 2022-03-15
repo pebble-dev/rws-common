296e730faf1aa4438bd50239f0d81fddaee8681f
Joshua Wise <joshua@joshuawise.com>
2022-03-14 21:11:12 -0400

    honeycomb: emit a traceresponse header

--- rws_common/honeycomb.py
+++ rws_common/honeycomb.py
@@ -1,3 +1,5 @@
+import threading
+
 from flask import request
 
 import beeline
@@ -12,6 +14,8 @@ sample_routes = {
 debug_tokens = {}
 
 def_sample_rate = 2
+
+_local = threading.local()
 
 def _sampler(fields):
     sample_rate = def_sample_rate
@@ -40,6 +44,53 @@ def _sampler(fields):
         return True, sample_rate
     return False, 0
 
+# The flow of the traceresponse wrapper is as follows:
+#
+#   TraceResponseWrapOuterWSGIMiddleware.call runs
+#     the Honeycomb middleware sets up a tracing context
+#       TraceResponseWrapInnerWSGIMiddleware runs, and stashes the current span
+#         the application runs
+#     the Honeycomb middleware tears down the span, and submits it
+#       the presend hook marks the current span as having been submitted, if the sampler did not eat it
+#     TraceResponseWrapOuterWSGIMiddleware.call._start_response reads the stashed span and submit state, and creates a header
+#   the Werkzeug (or whatever) infrastructure emits headers back to the world
+#
+# Ideally, this should live as part of the Beeline.  For now, we put it
+# here, since we control this and we don't control the Beeline...
+
+class TraceResponseWrapInnerWSGIMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        span = None
+        bl = beeline.get_beeline()
+        if bl:
+            span = bl.tracer_impl.get_active_span()
+        _local.current_span = span
+        _local.pending = True
+        return self.app(environ, start_response)
+
+def _presend(fields):
+    if _local.current_span.id == fields['trace.span_id']:
+        _local.pending = False
+
+class TraceResponseWrapOuterWSGIMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        def _start_response(status, headers, *args):
+            span = _local.current_span
+            if span:
+                headers.append(('traceresponse', f"00-{span.trace_id.replace('-','')}-{span.id.replace('-','')[-16:]}-{'00' if _local.pending else '01'}"))
+                _local.current_span = None
+            return start_response(status, headers, *args)
+
+        return self.app(environ, _start_response)
+
 def init(app, service):
-    beeline.init(service_name = service, sampler_hook=_sampler)
+    beeline.init(service_name = service, sampler_hook=_sampler, presend_hook=_presend)
+    app.wsgi_app = TraceResponseWrapInnerWSGIMiddleware(app.wsgi_app)
     HoneyMiddleware(app, db_events=True)
+    app.wsgi_app = TraceResponseWrapOuterWSGIMiddleware(app.wsgi_app)
